0f4a526b41b0a3fc0e33cb90b35030f54e9d4c31
Nello Casolla <nellocasolla446@gmail.com>
2023-06-08 14:24:27 +0200

    assets

---
+++ src/test/java/com/example/u4d23/U4D23ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.u4d23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U4D23ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
